9f32ec2b04cc88bbbfbc317acb9ea0d5fdc59496
Himalaya vats <contact@himalayavats.com>
2021-11-25 22:31:09 +0530

    Removed the dev page

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.get('/blog', (req, res) => {
     res.send('<h1>I am blog page</h1>');
 }); 
 
-app.get('/dev', (req, res) => {
-    res.send('<h1>I am a dev page</h1>');
-}); 
-
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 });
